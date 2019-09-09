592f3264098ddd1fe53314a5cc43df07d3877631
heiwa4126 <heiwa4126@gmail.com>
2019-09-09 15:07:14 +0900

    Third release test

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-GOOS=linux   GOARCH=amd64 go build -o goHome0           -ldflags="-w -s" main.go
-GOOS=windows GOARCH=386   go build -o goHome0-386.exe   -ldflags="-w -s" main.go
-GOOS=windows GOARCH=amd64 go build -o goHome0-amd64.exe -ldflags="-w -s" main.go
+GOOS=linux   GOARCH=amd64 go build -o goHome0-linux-amd64       -ldflags="-w -s" main.go
+GOOS=windows GOARCH=386   go build -o goHome0-windows-386.exe   -ldflags="-w -s" main.go
+GOOS=windows GOARCH=amd64 go build -o goHome0-windows-amd64.exe -ldflags="-w -s" main.go
 # upx --best goHome0 goHome0.exe
-# upx goHome0 goHome0.exe
+upx *.exe goHome0-linux-amd64
